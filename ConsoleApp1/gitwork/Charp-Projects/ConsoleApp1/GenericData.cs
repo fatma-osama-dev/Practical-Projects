34ccf7514c733a364ff138f46fedf7ed258d8a6f
Fatma Osama <fatma.osama.dev@gmail.com>
2026-05-11 17:08:26 +0300

    Upload CSharp-Projects folder as a whole

---
+++ ConsoleApp1/gitwork/Charp-Projects/ConsoleApp1/GenericData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class GenericData<T> where T : class, new()
+    {
+        public T data { get; set; }
+
+        public void print(T data) {
+            Console.WriteLine(data);
+        
+        }
+    }
+}
